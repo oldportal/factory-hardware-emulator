f9f6000a4401ad3ab3865bc89d1fd0127295952d
Dmitry Ognyannikov <sirius_plus@yahoo.com>
2014-10-18 22:12:52 -0200

    Modbus request handler

--- factory-hardware-emulator/hardware/modbus/_group_modbus.h
+++ factory-hardware-emulator/hardware/modbus/_group_modbus.h
@@ -7,7 +7,7 @@
 
 //BEGIN_GROUP_COMMENT
 /**
-Modbus communication interface data structures (modbus registers represent*/
+Modbus communication interface data structures (modbus registers represen*/
 //END_GROUP_COMMENT
 
 
